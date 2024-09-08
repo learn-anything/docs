c7afbf3d176c3cf58ddffc7a6330e2f379323318
Nikita <github@nikiv.dev>
2024-09-08 17:11:25 +0300

    .

--- app/source.ts
+++ app/source.ts
@@ -3,6 +3,6 @@ import { createMDXSource } from "fumadocs-mdx"
 import { loader } from "fumadocs-core/source"
 
 export const source = loader({
-	baseUrl: "/docs",
+	baseUrl: "/",
 	source: createMDXSource(docs, meta)
 })
